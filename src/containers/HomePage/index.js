894b6bce5e9064099b37b462f73f6e35bab34800
hamdi <76883216+charradihamdi@users.noreply.github.com>
2022-03-10 16:17:55 +0100

    first commit

---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+/**
+* @author
+* @function HomePage
+**/
+
+const HomePage = (props) => {
+  return(
+    <h1>
+      Home Page
+    </h1>
+   )
+
+ }
+
+export default HomePage
